0dc8c55dbae59eb03d5baedf5f5d4b02026e0361
Ivo Nunes <inunes1904@gmail.com>
2023-07-11 23:10:29 +0100

    first commit

---
+++ src/main/java/com/ivonunes/exercisessb/ExercisesSbApplication.java
@@ -0,0 +1,13 @@
+package com.ivonunes.exercisessb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExercisesSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExercisesSbApplication.class, args);
+	}
+
+}
